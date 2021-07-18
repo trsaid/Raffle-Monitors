c79e513a545e95a5d8043ad1c9a615bd443c99b9
RedStewart <reedostewart@gmail.com>
2021-07-18 15:37:53 +1200

    Added BSTN

---
+++ Sites/BSTN/sendWebhook.js
@@ -0,0 +1,59 @@
+const Config = require('../../config/Config.json');
+const Logger = require('../../tools/Logger');
+
+const axios = require('axios');
+
+const sendWebhook = async (data, site) => {
+  try {
+    const webhookFormat = {
+      embeds: [
+        {
+          color: 5301186,
+          fields: [
+            { name: '**Title**', value: data.title, inline: true },
+            { name: '**Store**', value: site, inline: true },
+            { name: '\u200b', value: '\u200b', inline: true }, // spacing field
+            { name: '**End Date**', value: data.endDate, inline: true },
+            { name: '**Locale**', value: data.locale, inline: true },
+            {
+              name: '**URL**',
+              value: data.url
+            }
+          ],
+          author: {
+            name: data.title,
+            url: data.url,
+            icon_url: 'https://www.fashionsauce.com/img/stores/bstn-store.png'
+          },
+          footer: {
+            text: '@trippiereedd',
+            icon_url:
+              'https://cdn.discordapp.com/avatars/175804433790140417/f6c30aaa4270fab75b5bc0763f702048.webp?size=128'
+          },
+          timestamp: new Date().toISOString(),
+          image: {
+            url: data.imgURL
+          }
+        }
+      ],
+      username: `${site} Monitor`,
+      avatar_url: 'https://www.fashionsauce.com/img/stores/bstn-store.png'
+    };
+
+    const options = {
+      method: 'POST',
+      url: Config.webhookURL,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: webhookFormat
+    };
+
+    axios(options);
+  } catch (err) {
+    Logger.error(err);
+    Logger.error('Failed to send webhook');
+  }
+};
+
+module.exports = sendWebhook;
